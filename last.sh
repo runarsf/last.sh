d66186a3b08625aa9af1ed314d80c349d24286d8
Runar Fredagsvik <runar.fredagsvik@gmail.com>
2019-02-08 09:51:20 +0100

    fixed jq error

--- last.sh
+++ last.sh
@@ -1,92 +1,36 @@
 #!/bin/bash
 
-COLOR_RED='\033[0;31m'
-COLOR_GREEN='\033[1;32m'
-COLOR_ORANGE='\033[0;33m'
-COLOR_YELLOW='\033[1;33m'
-COLOR_PURPLE='\033[1;35m'
-COLOR_CYAN='\033[1;36m'
-COLOR_NONE='\033[0m'
+C_RED='\033[0;31m'
+C_GREEN='\033[1;32m'
+C_ORANGE='\033[0;33m'
+C_YELLOW='\033[1;33m'
+C_PURPLE='\033[1;35m'
+C_CYAN='\033[1;36m'
+C_NONE='\033[0m'
 
 usage () {
-	printf "\n\t ${COLOR_GREEN}"
+	printf "\n\t ${C_GREEN}"
 	printf "\n\t██╗     ███████╗███████╗████████╗███████╗██╗   ██╗"
 	printf "\n\t██║     ██╔══██║██╔════╝ ╚═██╔══╝██╔════╝██║   ██║"
 	printf "\n\t██║     ███████║███████╗   ██║   ███████╗████████║"
 	printf "\n\t██║     ██╔══██║╚════██║   ██║   ╚════██║██╔═══██║"
-	printf "\n\t███████╗██║  ██║███████║   ██║${COLOR_CYAN}██╗${COLOR_GREEN}███████║██║   ██║"
-	printf "\n\t╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝${COLOR_CYAN}╚═╝${COLOR_GREEN}╚══════╝╚═╝   ╚═╝"
-	printf "\n${COLOR_NONE}"
+	printf "\n\t███████╗██║  ██║███████║   ██║${C_CYAN}██╗${C_GREEN}███████║██║   ██║"
+	printf "\n\t╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝${C_CYAN}╚═╝${C_GREEN}╚══════╝╚═╝   ╚═╝"
+	printf "\n${C_NONE}"
 	printf "\n\t Usage: last.sh [args]"
 	printf "\n\t\t -r\t\tRun"
 	printf "\n\t\t -h\t\tHelp"
 	printf "\n\t\t -c\t\tCredentials setup."
 	printf "\n\t\t -d\t\tDelete credentials."
-	printf "\n\t\t -a\t\tRelay API information. Returns formatted reply in cleartext."
-	printf "\n\t\t -q\t\tRelay API information. Returns json."
 	printf "\n\t\t -p\t\tPolybar mode, returns output once."
 	printf "\n\t\t -u\t\tDefine a custom user that is not saved as credentials."
-	printf "\n\n${COLOR_NONE}"
+	printf "\n\n${C_NONE}"
 }
 
 customUser () {
 	credentials
 	LASTFM_USER=${OPTARG}
 	runner
-}
-
-polybar () {
-	source /etc/environment
-	if [[ $LASTFM_USER == "" ]] || [[ $LASTFM_API_KEY == "" ]]; then
-		exit 1
-	fi
-
-	response=`curl -s "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=$LASTFM_USER&api_key=$LASTFM_API_KEY&format=json"`
-	artist=`echo "$response" | jq ".[] | .track | .[0] | .artist " | tr -d "\#" | jq ".text" | tr -d '"'`
-	track=`echo "$response" | jq ".[] | .track | .[0] | .name" | tr -d '"'`
-	album=`echo "$response" | jq ".[] | .track | .[0] | .album " | tr -d "\#" | jq ".text" | tr -d '"'`
-	curr="$track - $artist"
-
-	isPlaying=`echo "$response" | jq '.[] | .track | .[0] | .["@attr"] | .nowplaying' | tr -d '"'`
-	if [[ $isPlaying == true ]]; then
-		echo "$curr"
-	else
-		echo ""
-	fi
-}
-
-ctapi () {
-	response=`curl -s "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=$LASTFM_USER&api_key=$LASTFM_API_KEY&format=json"`
-	if [[ "${OPTARG}" == "track" ]]; then
-		track=`echo "$response" | jq ".[] | .track | .[0] | .name" | tr -d '"'`
-		printf "$track"
-		exit 0
-	elif [[ "${OPTARG}" == "artist" ]]; then
-		artist=`echo "$response" | jq ".[] | .track | .[0] | .artist" | tr -d "\#" | jq ".text" | tr -d '"'`
-		printf "$artist"
-		exit 0
-	elif [[ "${OPTARG}" == "album" ]]; then
-		album=`echo "$response" | jq ".[] | .track | .[0] | .album" | tr -d "\#" | jq ".text" | tr -d '"'`
-		printf "$album"
-		exit 0
-	elif [[ "${OPTARG}" == "url" ]]; then
-		url=`echo "$response" | jq ".[] | .track | .[0] | .url" | tr -d '"'`
-		printf "$url"
-		exit 0
-	fi
-}
-
-jqapi () {
-	response=`curl -s "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=$LASTFM_USER&api_key=$LASTFM_API_KEY&format=json"`
-
-	if [[ "${OPTARG}" == "pure" ]]; then
-		printf "$response"
-		exit 0
-	elif [[ "${OPTARG}" == "track" ]]; then
-		track=`echo "$response" | jq ".[] | .track | .[0]" | tr -d '"'`
-		printf "$track"
-		exit 0
-	fi
 }
 
 animate () {
@@ -106,9 +50,9 @@ credentials () {
 }
 
 getCredentials () {
-	printf "${COLOR_GREEN}"
+	printf "${C_GREEN}"
 	read -p "Username: " LASTFM_USER
-	printf "${COLOR_RED}"
+	printf "${C_RED}"
 	read -sp "API Key: " LASTFM_API_KEY
 
 	printf "\n\n"
@@ -117,7 +61,7 @@ getCredentials () {
 		y|Y) saveCredentials;;
 	esac
 
-	printf "${COLOR_CYAN}\nWelcome, $LASTFM_USER!\n${COLOR_NONE}"
+	printf "${C_CYAN}\nWelcome, $LASTFM_USER!\n${C_NONE}"
 }
 
 saveCredentials () {
@@ -135,14 +79,14 @@ saveCredentials () {
 		sudo echo -e "\nLASTFM_API_KEY=\"$LASTFM_API_KEY\"" >> /etc/environment
 	fi
 	sed -i '/^$/d' /etc/environment
-	printf "${COLOR_GREEN}Credentials saved!${COLOR_NONE}"
+	printf "${C_GREEN}Credentials saved!${C_NONE}"
 }
 
 deleteCredentials () {
 	sudo sed -i 's/.*LASTFM_USER.*//g' /etc/environment
 	sudo sed -i 's/.*LASTFM_API_KEY.*//g' /etc/environment
 	sed -i '/^$/d' /etc/environment
-	printf "${COLOR_ORANGE}Credentials deleted!${COLOR_NONE}"
+	printf "${C_ORANGE}Credentials deleted!${C_NONE}"
 }
 
 runner () {
@@ -150,32 +94,62 @@ runner () {
 	while true; do
 		animate
 		response=`curl -s "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=$LASTFM_USER&api_key=$LASTFM_API_KEY&format=json"`
-		artist=`echo "$response" | jq ".[] | .track | .[0] | .artist " | tr -d "\#" | jq ".text" | tr -d '"'`
-		track=`echo "$response" | jq ".[] | .track | .[0] | .name" | tr -d '"'`
-		album=`echo "$response" | jq ".[] | .track | .[0] | .album " | tr -d "\#" | jq ".text" | tr -d '"'`
-		curr="--> $artist : $track ( $album )"
 
-		isPlaying=`echo "$response" | jq '.[] | .track | .[0] | .["@attr"] | .nowplaying' | tr -d '"'`
-		if [[ $isPlaying != true ]]; then
-			echo -en "\033[2K"
-			prev="$curr"
-		elif [[ $isPlaying == true ]]; then
-			echo -en "\033[2K"
-			if (( ${#curr} > `tput cols` )); then
-				curr="--> $artist : $track"
+		if echo "$response" | jq ".[] | .track | .[0] | .name" | tr -d '"'  > /dev/null; then
+			artist=`echo "$response" | jq ".[] | .track | .[0] | .artist " | tr -d "\#" | jq ".text" | tr -d '"'`
+			track=`echo "$response" | jq ".[] | .track | .[0] | .name" | tr -d '"'`
+			album=`echo "$response" | jq ".[] | .track | .[0] | .album " | tr -d "\#" | jq ".text" | tr -d '"'`
+			curr="--> $artist : $track ( $album )"
+
+			isPlaying=`echo "$response" | jq '.[] | .track | .[0] | .["@attr"] | .nowplaying' | tr -d '"'`
+			if [[ $isPlaying != true ]]; then
+				echo -en "\033[2K"
+				prev="$curr"
+			elif [[ $isPlaying == true ]]; then
+				echo -en "\033[2K"
 				if (( ${#curr} > `tput cols` )); then
-					curr="--> $track"
+					curr="--> $artist : $track"
 					if (( ${#curr} > `tput cols` )); then
-						curr="--> Error: README.rst"
+						curr="--> $track"
+						if (( ${#curr} > `tput cols` )); then
+							curr="--> Error: README"
+						fi
 					fi
 				fi
+				echo -en "$curr\r"
+				prev="$curr"
 			fi
-			echo -en "$curr\r"
-			prev="$curr"
+		else
+			echo "track lookup failed"
 		fi
 		animate
 		sleep 0.1;
 	done
+}
+
+polybar () {
+	source /etc/environment
+	if [[ $LASTFM_USER == "" ]] || [[ $LASTFM_API_KEY == "" ]]; then
+		exit 1
+	fi
+
+	response=`curl -s "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=$LASTFM_USER&api_key=$LASTFM_API_KEY&format=json"`
+
+	if echo "$response" | jq ".[] | .track | .[0] | .name" | tr -d '"'  > /dev/null; then
+		artist=`echo "$response" | jq ".[] | .track | .[0] | .artist " | tr -d "\#" | jq ".text" | tr -d '"'`
+		track=`echo "$response" | jq ".[] | .track | .[0] | .name" | tr -d '"'`
+		album=`echo "$response" | jq ".[] | .track | .[0] | .album " | tr -d "\#" | jq ".text" | tr -d '"'`
+		curr="$track - $artist"
+
+		isPlaying=`echo "$response" | jq '.[] | .track | .[0] | .["@attr"] | .nowplaying' | tr -d '"'`
+		if [[ $isPlaying == true ]]; then
+			echo "$curr"
+		else
+			echo ""
+		fi
+	else
+		echo "track lookup failed"
+	fi
 }
 
 while getopts "rdchpu:a:q: " arg; do
@@ -184,8 +158,6 @@ while getopts "rdchpu:a:q: " arg; do
 		d) deleteCredentials;;
 		c) getCredentials;;
 		p) polybar;;
-		a) credentials; ctapi;;
-		q) credentials; jqapi;;
 		u) customUser;;
 		h|*) usage;;
 	esac
